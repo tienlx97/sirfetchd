636799fc194f46e1ba126111889c94ca914e198a
lexuantien <tienlx97@gmail.com>
2022-04-20 22:12:05 +0700

    Update utils *.d.ts

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -2,7 +2,13 @@ import React from "react";
 // const React = require("react");
 const ReactDOM = require("react-dom");
 
-import { BaseRowItem_Test1 } from "@farfetchd/baserow/__tests__/BaseRowItem.test"
+// import { BaseRowItem_Test1 } from "@farfetchd/baserow/__tests__/BaseRowItem.test"
+
+import { performanceNow } from "@farfetchd/utils"
+const GlobalDeclare = () => {
+  const now = performanceNow()
+  return <div>{now}</div>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<BaseRowItem_Test1 />);
+root.render(<GlobalDeclare />);
